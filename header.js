7492bd8b2a061ceed7fcd9c5485de2607aacc333
Mame-dev <mameseynaboudiagne@yahoo.fr>
2021-07-22 22:49:30 +0200

    Ajout Fichier zurss-50S.html

--- header.js
+++ header.js
@@ -4,5 +4,7 @@ navToggle.addEventListener("click",function()
 {if(navWrapper.classList.contains("active"))
 {this.setAttribute("aria-expanded","false");
 this.setAttribute("aria-label","menu");
-navWrapper.classList.remove("active");}else{navWrapper.classList.add("active");
-this.setAttribute("aria-label","close menu");this.setAttribute("aria-expanded","true");}});
+navWrapper.classList.remove("active");}
+else{navWrapper.classList.add("active");
+this.setAttribute("aria-label","close menu");
+this.setAttribute("aria-expanded","true");}});
