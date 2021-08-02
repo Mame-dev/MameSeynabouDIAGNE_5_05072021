684bca80005d27ee31fae85ea0998d83172f92f6
Mame-dev <mameseynaboudiagne@yahoo.fr>
2021-08-02 11:10:43 +0200

    Modification page index.html et ajouts fichiers confirmation.html, confirmation.js, panier.js, produits.js, panier.html, produit.html

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let container = document.getElementById('container');
+/*let container = document.getElementById('row container');
 
 // Aller cherche URL
 fetch('http://localhost:3000/api/cameras')
@@ -14,13 +14,14 @@ fetch('http://localhost:3000/api/cameras')
         console.log(camera);
         console.log(camera.name);
         //creation nouveau élément div
-        let bloc = document.createElement("div");
+        /*let bloc = document.createElement('bloc');
+        //let bloc = document.getElementById('bloc');
         //contenu élément div
         bloc.innerHTML +=
-        `<div class="bloc">
+        `<div class="col-sm-6 bloc">
             <img src="http://localhost:3000/images/vcam_1.jpg" alt="Zurss 50S">
-            <div class="text">
-                <h2 class="nom">$name</h2>
+            <div class="col text">
+                <h2 class="nom">${'produit.name'}</h2>
                 <h2 class="prix">price€</h2>
             </div>
             <p class="description">
@@ -37,5 +38,39 @@ fetch('http://localhost:3000/api/cameras')
         container.appendChild(bloc);
     })
     
-})
+})*/
 
+//Fonction principale, gere le temps de telechargement
+(async function() {
+    const articles = await getArticles()
+    //Creation de la boucle des articles
+    for (article of articles) {
+        displayArticle(article)
+    }
+    })()
+    async function getArticles(){
+    try {
+        //Appelle la reponse de l'api
+        const httpBodyResponse = await fetch("http://localhost:3000/api/cameras")
+        const articles = await httpBodyResponse.json()
+        return articles
+    } catch (error) {
+        alert(error)
+    }
+}
+    //Gere laffichage des produits
+    function displayArticle(article){
+    //Appelle le temmplate
+    const templateElt = document.getElementById("templateArticle")
+    //Clone le template
+    const cloneElt = document.importNode(templateElt.content, true)
+
+    cloneElt.getElementById("name").textContent = article.name
+    cloneElt.getElementById("price").textContent = `${article.price / 100}.00 €`
+    cloneElt.getElementById("description").textContent = article.description
+    cloneElt.getElementById("imageUrl").src = article.imageUrl
+    cloneElt.getElementById('articleLink').href = `product.html?id=${article._id}`
+    //Affiche lelement enfant clone
+    
+    document.getElementById("main").appendChild(cloneElt)
+}
